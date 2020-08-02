1685f67bbdbfd56d28fed91653a1bd02b07c5d88
niranjan-n <niranjan.n@evolvussolutions.com>
2020-08-02 21:21:52 +0530

    recipes and shopping setup addded

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pasta','Recipe for pasta','https://pinchofyum.com/wp-content/uploads/Vegan-Vodka-Pasta-Square.jpg')
+    ,new Recipe('Pizza','Recipe for pizza','https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=h30EAnkk')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
