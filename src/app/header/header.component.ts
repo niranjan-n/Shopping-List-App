7ff9aa5aeaf1e21e2619a051b0cd7f73d2e4a7dd
niranjan-n <hegdeniranjan97@gmail.com>
2020-08-09 18:41:43 +0530

    passing recipe data  with property binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output()  loadedFeature=new EventEmitter<String>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onClick(feature:String){
+    this.loadedFeature.emit(feature);
   }
 
 }
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeItemComponent implements OnInit {
 
   constructor() { }
-
+  @Input() recipe: Recipe;
   ngOnInit(): void {
   }
 
