79bff0cecb9a979bc3f5b3fac153b6a58cd59554
niranjann-evolvus <niranjan.n@evolvussolutions.com>
2020-08-09 19:17:45 +0530

    Passing Data with Event and

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter,Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
-
+  @Input() recipe =new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -7,13 +7,17 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
+    recipes: Recipe[] = [
     new Recipe('Pasta','Recipe for pasta','https://pinchofyum.com/wp-content/uploads/Vegan-Vodka-Pasta-Square.jpg')
     ,new Recipe('Pizza','Recipe for pizza','https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=h30EAnkk')
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  recipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
